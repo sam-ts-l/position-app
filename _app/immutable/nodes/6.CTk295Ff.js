905dd74a66d7df491b71dbddab15ede43fe4d931
TushenLan <s4137286@student.rmit.edu.au>
2024-10-19 16:01:36 +1100

    Updates

---
+++ _app/immutable/nodes/6.CTk295Ff.js
@@ -0,0 +1,9 @@
+import{s as n,f as r,g as l,y as c,j as h,i as d,z as a,d as m}from"../chunks/scheduler.Cyh6NhFR.js";import{S as p,i as g}from"../chunks/index.CEB8LcsN.js";function u(o){let e,i=`<h1 class="text-4xl font-bold mb-6">Story Line and Game Context</h1> <h2 class="text-2xl font-semibold mb-4">Year: 2030</h2> <p class="mb-6">A catastrophic global cyber-attack has wiped out most of the world’s digital data, 
+        leaving cities without the digital infrastructure that once guided explorers and tourists. 
+        Even major platforms like Google Maps and OpenStreetMap have lost their data. In this 
+        post-attack world, navigation has become a challenge, and the rich history of cities 
+        like Melbourne is at risk of being forgotten.</p> <h2 class="text-2xl font-semibold mb-4">The Player’s Mission:</h2> <p class="mb-6">As a Melbourne local, you are one of the few who remember the hidden gems and iconic 
+        sites of this once-thriving city. Your mission is to help rebuild Melbourne’s digital
+        map by rediscovering and documenting its significant locations, landmarks, and cultural
+        heritage. You must help Melbourne reappear on the global map so that people can explore it
+        once more, bringing life and activity back to the city.</p>`;return{c(){e=r("div"),e.innerHTML=i,this.h()},l(t){e=l(t,"DIV",{class:!0,"data-svelte-h":!0}),c(e)!=="svelte-nn10km"&&(e.innerHTML=i),this.h()},h(){h(e,"class","container mx-auto p-6 bg-gray-100 text-gray-800")},m(t,s){d(t,e,s)},p:a,i:a,o:a,d(t){t&&m(e)}}}class x extends p{constructor(e){super(),g(this,e,null,u,n,{})}}export{x as component};
