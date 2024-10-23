018fc811c80d91acb966ee91fdf7673974945cc0
TushenLan <s4137286@student.rmit.edu.au>
2024-10-23 11:29:26 +1100

    Updates

--- _app/immutable/chunks/paths.CxiAwh2g.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_19t28k3)==null?void 0:s.base)??"/position-app";var t;const e=((t=globalThis.__sveltekit_19t28k3)==null?void 0:t.assets)??a;export{e as a,a as b};
--- _app/immutable/nodes/6.CL9FMpls.js
+++
@@ -1,9 +0,0 @@
-import{s as n,n as a}from"../chunks/scheduler.DtGJeGpk.js";import{S as r,i as l,g as c,h,y as d,k as m,a as p,f as g}from"../chunks/index.DafFINNn.js";function u(o){let e,i=`<h1 class="text-4xl font-bold mb-6">Story Line and Game Context</h1> <h2 class="text-2xl font-semibold mb-4">Year: 2030</h2> <p class="mb-6">A catastrophic global cyber-attack has wiped out most of the world’s digital data, 
-        leaving cities without the digital infrastructure that once guided explorers and tourists. 
-        Even major platforms like Google Maps and OpenStreetMap have lost their data. In this 
-        post-attack world, navigation has become a challenge, and the rich history of cities 
-        like Melbourne is at risk of being forgotten.</p> <h2 class="text-2xl font-semibold mb-4">The Player’s Mission:</h2> <p class="mb-6">As a Melbourne local, you are one of the few who remember the hidden gems and iconic 
-        sites of this once-thriving city. Your mission is to help rebuild Melbourne’s digital
-        map by rediscovering and documenting its significant locations, landmarks, and cultural
-        heritage. You must help Melbourne reappear on the global map so that people can explore it
-        once more, bringing life and activity back to the city.</p>`;return{c(){e=c("div"),e.innerHTML=i,this.h()},l(t){e=h(t,"DIV",{class:!0,"data-svelte-h":!0}),d(e)!=="svelte-nn10km"&&(e.innerHTML=i),this.h()},h(){m(e,"class","container mx-auto p-6 bg-gray-100 text-gray-800")},m(t,s){p(t,e,s)},p:a,i:a,o:a,d(t){t&&g(e)}}}class x extends r{constructor(e){super(),l(this,e,null,u,n,{})}}export{x as component};
